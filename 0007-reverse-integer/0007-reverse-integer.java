aee988646c7652f5ca22b1970a3f848d726e8ea8
Tanmay Pradhan <tanmaypradhan394@gmail.com>
2026-07-01 09:03:14 +0530

    Time: 1 ms (99.98%), Space: 42.4 MB (79.7%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverse(int x) {
+        long num = 0;
+        while(x != 0){
+            int digit = x % 10;
+            num = num * 10 + digit;
+            x /= 10;
+        }
+        if(num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) return 0;
+        return (int) num;
+    }
+}
