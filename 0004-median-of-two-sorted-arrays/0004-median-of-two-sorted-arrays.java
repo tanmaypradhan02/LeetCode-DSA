e3c5a8751ac586ea1413d7a09574e174d238acd3
Tanmay Pradhan <tanmaypradhan394@gmail.com>
2026-09-26 10:23:04 +0530

    Time: 1 ms (100%), Space: 48.9 MB (32.37%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,57 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // Always binary search on the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int m = nums1.length;
+        int n = nums2.length;
+
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+
+            // Partition nums1
+            int cut1 = low + (high - low) / 2;
+
+            // Partition nums2
+            int cut2 = (m + n + 1) / 2 - cut1;
+
+            // Boundary values
+            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int right1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+
+            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int right2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd total length
+                if ((m + n) % 2 == 1) {
+                    return Math.max(left1, left2);
+                }
+
+                // Even total length
+                int leftMax = Math.max(left1, left2);
+                int rightMin = Math.min(right1, right2);
+
+                return (leftMax + rightMin) / 2.0;
+            }
+
+            // We took too many elements from nums1
+            else if (left1 > right2) {
+                high = cut1 - 1;
+            }
+
+            // We took too few elements from nums1
+            else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0;
+    }
+}
